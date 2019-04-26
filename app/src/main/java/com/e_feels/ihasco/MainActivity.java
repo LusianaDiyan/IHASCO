a4c3c7eafd35966ef9fdf2e729bf3ff1f955f3d1
Pravasta Caraka <43958791+pravastacaraka@users.noreply.github.com>
2019-04-26 13:10:50 +0700

    add PairGlassActivity

--- app/src/main/java/com/e_feels/ihasco/MainActivity.java
+++ app/src/main/java/com/e_feels/ihasco/MainActivity.java
@@ -1,5 +1,6 @@
 package com.e_feels.ihasco;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         btnEng.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(intent);
             }
         });
     }
