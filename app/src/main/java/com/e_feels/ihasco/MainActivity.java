02f24fa6b951b180369f39a61f2077143fbd025a
Pravasta Caraka <43958791+pravastacaraka@users.noreply.github.com>
2019-04-26 13:41:34 +0700

    add en/id language button

--- app/src/main/java/com/e_feels/ihasco/MainActivity.java
+++ app/src/main/java/com/e_feels/ihasco/MainActivity.java
@@ -1,8 +1,8 @@
 package com.e_feels.ihasco;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,7 +21,15 @@ public class MainActivity extends AppCompatActivity {
         btnEng.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                Intent intent = new Intent(MainActivity.this, com.e_feels.ihasco.lang.en.StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnInd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, com.e_feels.ihasco.lang.id.StartActivity.class);
                 startActivity(intent);
             }
         });
